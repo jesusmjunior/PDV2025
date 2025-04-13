b44364b583837757e3875a0ef5406686fbcf5c7f
Adm. Jesus <134287070+jesusmjunior@users.noreply.github.com>
2025-04-13 02:53:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # main.py
-# Entrada principal do app ORION PDV Modularizado
-
 import streamlit as st
 from auth import autenticar_usuario
 from cadastro_produto import render_cadastro_produto
@@ -9,16 +7,13 @@ from registro_venda import render_registro_venda
 from relatorios import render_relatorios
 from painel import render_painel
 
-# Inicializa estado de autenticação
 if "autenticado" not in st.session_state:
     st.session_state["autenticado"] = False
 
-# Login
 if not st.session_state["autenticado"]:
     autenticar_usuario()
     st.stop()
 
-# Menu lateral
 st.sidebar.title("🔹 Menu PDV")
 if st.sidebar.button("Sair"):
     st.session_state["autenticado"] = False
@@ -27,7 +22,6 @@ if st.sidebar.button("Sair"):
 menu = st.sidebar.radio("Escolha a opção:", [
     "Cadastro Produto", "Cadastro Cliente", "Registrar Venda", "Relatórios", "Painel"])
 
-# Navega entre os módulos
 if menu == "Cadastro Produto":
     render_cadastro_produto()
 elif menu == "Cadastro Cliente":
